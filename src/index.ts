2208dd7a66d43867cac8fc50708a586184fddbef
grakzanchetta <grakzanchetta@gmail.com>
2022-09-07 13:54:46 -0300

    lib setup

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+// import router from "dotenv";
+import dotenv from "dotenv";
+// import errorHandlerMiddleware from "./middlewares/errorHandler";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(json());
+// app.use(router);
+// app.use(errorHandlerMiddleware);
+
+const PORT: number = Number(process.env.PORT) || 5009;
+
+app.listen(PORT, () =>
+  console.log(`It's friday theeeeen Saturday Sunday WHAT?! on port ${PORT}`)
+);
